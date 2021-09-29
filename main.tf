6ebc6722da1419c7856974f147f8c2f29b509e5b
Bharat4317 <78852799+Bharat4317@users.noreply.github.com>
2021-09-29 15:01:22 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,16 @@
-1
+provider "azurerm" {
+  version = "~>2.0"
+  features {}
+}
+
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "smcp-terraform"
+    storage_account_name = "smcpterraform"
+    container_name       = "github-terrafrom"
+    key                  = "terraformgithubexample.tfstate"
+  }
+}
+
+
+
